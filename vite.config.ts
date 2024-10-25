75f6d07cd1fe1a39ad9e3c332750eb701c27e46c
pearl.vernon-howe <pearl.vernon-howe@twinkl.co.uk>
2024-10-25 17:20:05 +0100

    fix URI malformed error + update local port

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,15 @@ export default defineConfig(({mode }) => {
   return {
     plugins: [
       react(),
-      eslint({
-        failOnError: false,
-      })
+      // eslint({
+      //   failOnError: false,
+      // })
     ],
     define: {
       'process.env': {
         MAILERSEND_API_KEY: 'mlsn.a78347aa85ac2c0bc227f7a387b1319383a637f7942fe75d6bdcb2208fcabf7f',
       }
-    }
+    },
+    server: { port: 3000 },
   };
 });
