8c4b36ab9e4d4f1791fda20439ac9a01b57c5282
pearl.vernon-howe <pearl.vernon-howe@twinkl.co.uk>
2024-11-18 17:20:04 +0000

    BUILD

---
+++ docs/assets/index-BN7xz8-Y.js
@@ -0,0 +1,1 @@
+import{C as o}from"./Contact-BmNvLXgn.js";import"./index-DAleIASU.js";export{o as default};
---
+++ docs/assets/index-F7vUCUHw.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DAleIASU.js";import{A as r}from"./AppWrapper-CgFiWEIA.js";import"./image9-DbXFBAOF.js";const e=()=>t.jsx(r,{children:t.jsx("div",{children:"Get your tickets here"})}),p=e;export{p as default};
