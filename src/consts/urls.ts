233fa801a7e04af14180ec9eaea98a4e70e856dd
pearlolivia <pearlolivia@hotmail.co.uk>
2025-02-01 10:38:22 +0000

    add NR warm up ticket link

--- src/consts/urls.ts
+++ src/consts/urls.ts
@@ -4,7 +4,7 @@ export const urls = {
     gallery: '/gallery',
     merch: '/merchandise',
     reviews: 'https://www.skiddle.com/whats-on/Sheffield/SADACCA/Pink-Lemonade-Rollerdisco/38190847/?reviews=1',
-    tickets: 'https://skiddle.com/e/40423859',
+    tickets: 'https://www.skiddle.com/whats-on/Leeds/Sports-Arena-Leeds-Beckett-University/Northern-Roll-Warm-Up-X-Pink-Lemonade-Roller-Disco/40605413/',
     instagram: 'https://www.instagram.com/pinklemonade_rollerdisco?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
     facebook: 'https://www.facebook.com/profile.php?id=100092048483607',
 };
